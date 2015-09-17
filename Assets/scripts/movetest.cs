8b4dc96275ab97c55152ec45d999044d85d63a5d
Madeline Falacci <mfalacci@gmail.com>
2015-09-17 15:05:55 -0400

    class practice

---
+++ Assets/scripts/movetest.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movetest : MonoBehaviour {
+
+	public float moveSpeed = 1f;
+	public float turnSpeed = 200f; 
+	
+	// Update is called once per frame
+	void Update () {
+		//to make frame rate independent, usually multiply by "Time.deltaTime"
+		// 0.1 (10FPS) 0.01 (100FPS) 
+
+	if (Input.GetKey (KeyCode.W)) {
+			transform.position += new Vector3(0f, 0f, moveSpeed * Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			transform.Translate ( new Vector3(0f, 0f, -moveSpeed * Time.deltaTime));
+			                     }
+
+		if (Input.GetKey (KeyCode.A)) {
+			transform.eulerAngles += new Vector3(0f, -turnSpeed * Time.deltaTime, 0f);
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			transform.Rotate (0f, turnSpeed * Time.deltaTime, 0f);
+		}
+		if (Input.GetKey (KeyCode.Space)) {
+			transform.position += new Vector3 (0f, moveSpeed * Time.deltaTime, 0f);
+		}
+
+}
+}
---
+++ Assets/scripts/vectortest.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class vectortest : MonoBehaviour {
+	public Transform sphere; 
+	public Transform cube;
+
+
+	
+	// Update is called once per frame
+	void Update () {
+	if (Input.GetKey (KeyCode.U)) {
+			cube.position += new Vector3 (0f, 1f, 0f);
+		}
+		Debug.DrawLine (sphere.position, cube.position, Color.cyan);
+
+		Debug.DrawLine (Vector3.zero, new Vector3 (1f, 5f, 2f));
+		Debug.DrawLine (new Vector3(1f, 5f, 2f), Vector3.one);
+	}
+}
